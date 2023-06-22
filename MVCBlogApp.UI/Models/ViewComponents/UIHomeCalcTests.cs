4f2b4c6268040437b55a77de4677d58cc3776059
OmerFarukBATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2023-06-22 14:46:58 +0300

    UIHomeCalcTests viewcomponent 'i ayarlandı.

---
+++ MVCBlogApp.UI/Models/ViewComponents/UIHomeCalcTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCBlogApp.UI.Models.ViewComponents
+{
+    public class UIHomeCalcTests : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
